55a306ebea9fcdff0547cc2baca350ec343d6771
João Otavio <joaokorcz@hotmail.com>
2022-10-17 23:12:09 -0300

    added: created find by cep output dto

--- src/cep/dto/find-by-cep.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsCEP } from 'brazilian-class-validator';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class FindByCepDto {
-    @IsString()
-    @IsNotEmpty()
-    @IsCEP({ message: 'CEP inválido' })
-    cep_code: string;
-}
