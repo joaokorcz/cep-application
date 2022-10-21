c6eb71d7940acd83bd456f684161580501e33ebd
João Otavio <joaokorcz@hotmail.com>
2022-10-21 10:55:59 -0300

    changed: renaming shell script to download files and parse and update python script to parse into uniche csv file

--- dataset/parser.py
+++ dataset/parser.py
@@ -7,13 +7,8 @@ files = glob.glob("*.csv")
 for file in files:
     with open(file, "r") as source:
         lines = csv.reader(source)
-
-        # splita o nome do arquivo e pega apenas as iniciais do estado
-        # nomes possuem o padrao sp.cepaberto_parte1.csv
-        state_initial = file.split('.')
-        state_initial = state_initial[0]
         
-        with open(f'{state_initial}.csv', "a") as result:
+        with open('all_ceps.csv', "a") as result:
             writer = csv.writer(result)
             for columns in lines:
                 writer.writerow((columns[0], columns[1], columns[3], columns[4], columns[5]))
