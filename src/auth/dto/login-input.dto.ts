b8fea4bf25989ed8ff3caa43cfd31c3f77fbfcdf
João Otavio <joaokorcz@hotmail.com>
2022-10-24 16:49:26 -0300

    added: validation at login dto and output

--- src/auth/dto/login-input.dto.ts
+++ src/auth/dto/login-input.dto.ts
@@ -1,5 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class LoginInputDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    @IsEmail()
     email: string;
 
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
     password: string;
 }
--- src/auth/dto/login-output.dto.ts
+++ src/auth/dto/login-output.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginOutputDto {
+    @ApiProperty()
     token: string;
 }
