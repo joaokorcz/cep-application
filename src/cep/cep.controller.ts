7316b42bb283fbfd6afab48167f533bb60d37e89
João Otavio <joaokorcz@hotmail.com>
2022-10-24 17:15:18 -0300

    added: guard and validation of token in new find address route

--- src/cep/cep.controller.ts
+++ src/cep/cep.controller.ts
@@ -3,15 +3,19 @@ import {
     Controller,
     Get,
     Param,
+    UseGuards,
     UseInterceptors,
     UsePipes,
     ValidationPipe,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import {
     ApiBadRequestResponse,
+    ApiBearerAuth,
     ApiOkResponse,
     ApiOperation,
     ApiTags,
+    ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { CepService } from './cep.service';
 import { FindByCepInputDto } from './dto/find-by-cep-input.dto';
@@ -29,6 +33,20 @@ export class CepController {
     @ApiOkResponse({ description: 'Address found', type: FindByCepOutputDto })
     @ApiBadRequestResponse({ description: 'Informed cep_code not valid' })
     async findByCepCode(
+        @Param() params: FindByCepInputDto,
+    ): Promise<FindByCepOutputDto> {
+        return this.cepService.findByCepCode(params.cep_code);
+    }
+
+    @Get('/protected/:cep_code')
+    @UseGuards(AuthGuard('jwt'))
+    @UsePipes(new ValidationPipe())
+    @ApiBearerAuth()
+    @ApiOperation({ description: 'Get address from cep_code' })
+    @ApiOkResponse({ description: 'Address found', type: FindByCepOutputDto })
+    @ApiBadRequestResponse({ description: 'Informed cep_code not valid' })
+    @ApiUnauthorizedResponse({ description: 'Unauthorized, token invalid' })
+    async protectedFindByCepCode(
         @Param() params: FindByCepInputDto,
     ): Promise<FindByCepOutputDto> {
         return this.cepService.findByCepCode(params.cep_code);
