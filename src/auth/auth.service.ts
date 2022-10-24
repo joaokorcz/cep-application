d93b591f04cb025314cba5f743ecb53a19eaf64a
João Otavio <joaokorcz@hotmail.com>
2022-10-24 16:50:04 -0300

    added: implemented login route and service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'nestjs-prisma';
+import * as bcrypt from 'bcrypt';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { LoginInputDto } from './dto/login-input.dto';
-import { JwtStrategyService } from './jwt-strategy/jwt-strategy.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private readonly prismaService: PrismaService,
         private readonly usersService: UsersService,
-        private readonly jwtService: JwtStrategyService,
+        private readonly jwtService: JwtService,
     ) {}
+
+    async validateCredentials(email: string, password: string) {
+        const user = await this.usersService.findByEmail(email);
+
+        if (!user || !bcrypt.compareSync(password, user.password)) {
+            throw new ForbiddenException(
+                'user with this credentials not found',
+            );
+        }
+
+        return user;
+    }
+
+    async login(login_input_dto: LoginInputDto) {
+        const { email, password } = login_input_dto;
+
+        const user = await this.validateCredentials(email, password);
+
+        const payload = {
+            id: user.id,
+        };
+
+        return {
+            token: this.jwtService.sign(payload),
+        };
+    }
 }
