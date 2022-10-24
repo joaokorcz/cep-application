2783e035536b1c56df1ede65a6192acc6bd2f54c
João Otavio <joaokorcz@hotmail.com>
2022-10-24 14:58:07 -0300

    fix: e2e test to /

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
         await app.init();
     });
 
-    it('/ (GET)', () => {
+    it('/ (GET)', async () => {
         return request(app.getHttpServer())
             .get('/')
             .expect(200)
-            .expect('Hello World!');
+            .expect({ message: 'hello world from cep-application :)' });
     });
 });
